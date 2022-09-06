ffb0c90adb8df56fccf7886566ba71ee70767d30
kimbeomjun <gimb52043@gmail.com>
2022-09-06 16:21:30 +0900

    renewal

--- JS/list.js
+++ JS/list.js
@@ -1,32 +1,31 @@
+// new
 $(".newSlider").slick({
   dots: true,
-  infinite: false,
+  infinite: true,
   speed: 300,
-//   slidesToShow: 2,
-//   slidesToScroll: 2,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
+  nextArrow: false,
+  prevArrow: false,
+});
+
+// 카테고리별
+$(".cateDivideSlider").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  nextArrow: false,
+  prevArrow: false,
+  slidesToShow: 3,
+});
+
+// 액티비티별
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.03,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+  },
 });
