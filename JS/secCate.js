3d702bfcbd855566ece68b3575227a74cdaa914c
kimbeomjun <gimb52043@gmail.com>
2022-09-13 17:56:58 +0900

    renewal

--- JS/secCate.js
+++ JS/secCate.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
   $(".listTabContent > div").hide();
   $(".listTabNavi a")
     .click(function () {
-      $(".listTabContent > div").hide().filter(this.hash).stop().fadeIn();
+      $(".listTabContent > div").hide().filter(this.hash).stop().show();
       $(".listTabNavi a").removeClass("active");
       $(this).addClass("active");
     })
     .filter(":eq(0)")
     .click();
 
-  $(".listTabNavi a").click(function(e){
+  $(".listTabNavi a").click(function (e) {
     e.preventDefault();
   });
 });
