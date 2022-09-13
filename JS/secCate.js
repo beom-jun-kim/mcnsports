e949120c788801925756a1c0cc758055afc26919
kimbeomjun <gimb52043@gmail.com>
2022-09-13 17:10:08 +0900

    renewal

---
+++ JS/secCate.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $('.listTabContent > div').hide();
+    $('.listTabNavi a').click(function(){
+        $('.listTabContent > div').hide().filter(this.hash).stop().fadeIn();
+        $('.listTabNavi a').removeClass('active');
+        $(this).addClass('active');
+    }).filter(':eq(0)').click();
+});
+
+
