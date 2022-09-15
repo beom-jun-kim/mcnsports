213a05840435dfc0a40993300088ff37210ce993
kimbeomjun <gimb52043@gmail.com>
2022-09-15 17:48:38 +0900

    renewal

--- JS/list.js
+++ JS/list.js
@@ -1,42 +1,44 @@
-// new
-$(".newSlider").slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  nextArrow: false,
-  prevArrow: false,
-});
+$(document).ready(function () {
+  // new
+  $(".newSlider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    nextArrow: false,
+    prevArrow: false,
+  });
 
-// 카테고리별
-$(".cateDivideSlider").slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  nextArrow: false,
-  prevArrow: false,
-  slidesToShow: 3,
-  cssEase: "ease-out",
-});
+  // 카테고리별
+  $(".cateDivideSlider").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    nextArrow: false,
+    prevArrow: false,
+    slidesToShow: 3,
+    cssEase: "ease-out",
+  });
 
-// hot
-var swiperPrdList = new Swiper("#swiperPrdList .swiper-container", {
-  slidesPerView: "auto",
-  spaceBetween: 20,
-  freeMode: true,
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true,
-    snapOnRelease: false,
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 3,
+  // hot
+  var swiperPrdList = new Swiper("#swiperPrdList .swiper-container", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    freeMode: true,
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      draggable: true,
+      snapOnRelease: false,
     },
-    768: {
-      slidesPerView: 4,
+    breakpoints: {
+      640: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      1024: {
+        slidesPerView: 5,
+      },
     },
-    1024: {
-      slidesPerView: 5,
-    },
-  },
+  });
 });
