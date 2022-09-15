40617fa16b0a64bbe2484bbb9e9d9613320b1378
kimbeomjun <gimb52043@gmail.com>
2022-09-15 17:39:27 +0900

    renewal

--- JS/list.js
+++ JS/list.js
@@ -15,17 +15,28 @@ $(".cateDivideSlider").slick({
   nextArrow: false,
   prevArrow: false,
   slidesToShow: 3,
+  cssEase: "ease-out",
 });
 
-// 액티비티별
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1.03,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "progressbar",
+// hot
+var swiperPrdList = new Swiper("#swiperPrdList .swiper-container", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  freeMode: true,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+    snapOnRelease: false,
   },
-  navigation: {
-    nextEl: ".swiper-button-next",
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 5,
+    },
   },
 });
